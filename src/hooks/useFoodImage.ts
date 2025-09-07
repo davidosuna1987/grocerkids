aad1174bb0af340dabb094d2720f64a7ef4cb7e8
David Osuna <davidosuna1987@gmail.com>
2025-09-07 08:22:52 +0000

    Las imágenes siguen siendo las mismas

--- src/hooks/useFoodImage.ts
+++ src/hooks/useFoodImage.ts
@@ -2,13 +2,13 @@
 import { useCallback } from "react";
 
 const PEXELS_API_KEY = "mFJQuartb3tcHMQ03Bzl703zH68DCsiaoia479Ej6QZJGsVf4p9ChE2Z";
-const PIXABAY_API_KEY = process.env.PIXABAY_API_KEY || "9757535-b18ed3a4f3633ebe78fc6b4d3";
+const PIXABAY_API_KEY = process.env.NEXT_PUBLIC_PIXABAY_API_KEY;
 
 type Provider = "pexels" | "pixabay";
 
 export function useFoodImage() {
 
-  const getProductImage = useCallback(async (food: string, provider: Provider = "pexels"): Promise<string> => {
+  const getProductImage = useCallback(async (food: string, provider: Provider): Promise<string> => {
     const fallbackImage = `https://picsum.photos/400/400?random=${crypto.randomUUID()}`;
 
     if (!food) return fallbackImage;
@@ -28,12 +28,12 @@ export function useFoodImage() {
         headers = { Authorization: PEXELS_API_KEY };
       } else if (provider === "pixabay") {
          if (!PIXABAY_API_KEY) {
-          console.error("Pixabay API key is missing.");
+          console.error("Pixabay API key is missing. Make sure it's defined in .env as NEXT_PUBLIC_PIXABAY_API_KEY");
           return fallbackImage;
         }
         url = `https://pixabay.com/api/?key=${PIXABAY_API_KEY}&q=${encodeURIComponent(
           food
-        )}&lang=es&image_type=photo&per_page=3`; // Fetch 3 to find first one
+        )}&lang=es&image_type=photo&per_page=3`;
       }
 
       const response = await fetch(url, { headers });

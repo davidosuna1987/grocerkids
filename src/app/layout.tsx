a0d0f16eb86e8eb83adb50c23365dca20718e7be
David Osuna <davidosuna1987@gmail.com>
2025-09-08 10:20:17 +0200

    added favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <meta name="apple-mobile-web-app-title" content="Grocer Kids" />
       </head>
       <body className={cn("font-body antialiased", "min-h-screen bg-background")}>
         <ThemeProvider
